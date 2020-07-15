6e69b4ab4173fed75214b97f7f28711f53458edb
msuphako <55439852+msuphako@users.noreply.github.com>
2020-07-15 14:56:14 +0700

    fuck u tim

--- application/views/menu.php
+++ application/views/menu.php
@@ -24,7 +24,7 @@ Tbkk
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="<?php echo base_url()?>/assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+  <link href="<?php echo base_url()?>assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="<?php echo base_url()?>/assets/demo/demo.css" rel="stylesheet" />
   <link href="<?php echo base_url()?>/assets/css/jquery.dataTables.min.css" rel="stylesheet" />
@@ -53,6 +53,13 @@ Tbkk
     font-size:16px;
   }
 </style>
+
+<script>
+        $(document).ready(function() {
+          $(".hide-it").fadeOut(5000);
+});
+   
+    </script>
 <body class="layout layout-header-fixed">
   <div class="wrapper ">
   <div class="sidebar  " data-color="green"  >
--- application/views/user/add.php
+++ application/views/user/add.php
@@ -1,7 +1,14 @@
-<div class="content">
+<style>
+label{
+  color:#495057;
+  font-size:16px; 
+   font-weight: bold;
+}
+</style>
+  <div class="content">
         <div class="container-fluid">
           <div class="row">
-            <div class="col-md-3"></div>
+            <div class="col-3"></div>
             <div class="col-md-6">
               <div class="card">
                 <div class="card-header card-header-rose">
@@ -17,28 +24,28 @@
 
                   <div class="col-md-12">
                         <div class="form-group">
-                          <label class="bmd-label-floating" style="color:#495057;font-size:16px"> Username</label>
+                          <label class="bmd-label-floating"><b>Username</b> </label>
                           <input type="text" class="form-control" name="username" required>
                         </div>
                       </div>
 
                       <div class="col-md-12">
                         <div class="form-group">
-                          <label class="bmd-label-floating" style="color:#495057;font-size:16px">Password</label>
+                          <label class="bmd-label-floating"><b>Password</b> </label>
                           <input type="password" class="form-control" name="password" required>
                         </div>
                       </div>
 
                       <div class="col-md-12">
                         <div class="form-group">
-                          <label class="bmd-label-floating" style="color:#495057;font-size:16px">Firstname</label>
+                          <label class="bmd-label-floating"><b>Firstname</b> </label>
                           <input type="text" class="form-control" name="fname" required>
                         </div>
                       </div>
                 
                       <div class="col-md-12">
                         <div class="form-group">
-                          <label class="bmd-label-floating" style="color:#495057;font-size:16px">Lastname</label>
+                          <label class="bmd-label-floating"><b>Lastname</b> </label>
                           <input type="text" class="form-control" name="lname" required>
                         </div>
                       </div>
@@ -46,7 +53,7 @@
 
                       <div class="col-md-12">
                         <div class="form-group">
-                          <label class="bmd-label-floating" style="color:#495057;font-size:16px"  >Email</label>
+                          <label class="bmd-label-floating"  ><b>Email</b> </label>
                           <input type="email" class="form-control" name="email" required>
                         </div>
                       </div>
@@ -54,11 +61,11 @@
 
 
                   <div class="form-group">
-                      <label for="email-2" class="col-sm-3 col-md-4 control-label" >Select Gender</label>      
+                      <label for="email-2" class="col-sm-3 col-md-4 control-label" ><b>Select Gender</b></label>      
           
-                      <div class="col-sm-6 col-md-3">
+                      <div class="col-sm-6 col-md-6">
                    <select name="gender" class="form-control" required>
-                    <option value="">Select Gender</option>
+                    <option value="">- - - Select Gender - - - </option>
                       <option value="male">male</option>
                       <option value="female">female</option>
                        
@@ -67,11 +74,11 @@
                     </div>
 
                 <div class="form-group">
-                      <label for="email-2" class="col-sm-3 col-md-4 control-label">Select Group</label>      
+                      <label for="email-2" class="col-sm-3 col-md-4 control-label" ><b> Select Group</b></label>      
           
-                      <div class="col-sm-6 col-md-3">
+                      <div class="col-sm-6 col-md-6">
                    <select name="sug_id" class="form-control select2"  required>
-                    <option value="">Select Group</option>
+                    <option value=""> - - - Select Group - - - </option>
                     <?php foreach ($excLoadG as $r) {
                         ?>
                        <option value="<?php echo $r->sug_id ?>"><?php echo $r->name ?></option>
