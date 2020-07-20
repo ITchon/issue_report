fc65af03b35440877f35c5347c9645c4873d6917
msuphako <55439852+msuphako@users.noreply.github.com>
2020-07-20 14:56:56 +0700

    Ver 1

---
+++ application/controllers/Report.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Report extends CI_Controller {
+
+    function __construct() { 
+    
+        parent::__construct(); 
+        $this->load->helper('form');
+        $this->load->helper('url');
+        $this->load->database(); 
+        $this->load->model('model');
+        $this->model->CheckSession();
+        $menu['menu'] = $this->model->showmenu($this->session->userdata('sug_id'));
+        $sql =  "select * from sys_menus where order_no != 0 and enable != 0 ORDER BY order_no";
+        $query = $this->db->query($sql); 
+        $url = trim($this->router->fetch_class().'/'.$this->router->fetch_method()); 
+         $menu['mg']= $this->model->givemeid($url);
+         $menu['submenu']= $query->result();
+         
+         $this->load->view('menu',$menu);
+       
+    }
+    public function manage()
+    {   
+
+        $this->load->view('report/manage');//bring $data to user_data 
+        $this->load->view('footer');
+    }
+
+    public function add()
+    {   
+        //$this->model->CheckPermission($this->session->userdata('su_id'));
+        //$this->model->CheckPermissionGroup($this->session->userdata('sug_id'));
+        $sql='SELECT spj.pj_id,spj.pj_name FROM sys_issue as sis
+        INNER JOIN sys_projects as spj ON spj.pj_id = sis.pj_id 
+        INNER JOIN sys_owner as sow ON sow.owner_id = sis.owner_id
+        where sis.delete_flag != 0 ';
+        //$sql =  'SELECT * FROM sys_users ';
+        $query = $this->db->query($sql); 
+        $data['result'] = $query->result(); 
+
+        
+        $sqlSelG = "SELECT * FROM sys_projects WHERE delete_flag != 0";
+        $query = $this->db->query($sqlSelG); 
+        $data['result_p'] = $query->result(); 
+
+        $sqlSelG = "SELECT * FROM sys_owner";
+        $query = $this->db->query($sqlSelG); 
+        $data['result_own'] = $query->result(); 
+
+        $this->load->view('issue/add',$data);//bring $data to user_data 
+        $this->load->view('footer');
+    }
+
+    public function upload(){
+
+        if(!empty($_FILES['file']['name'])){
+     
+          // Set preference
+          $config['upload_path'] = 'uploads/'; 
+          $config['allowed_types'] = '*';
+          $config['max_size'] = '102400'; // max_size in kb
+          $config['file_name'] = $_FILES['file']['name'];
+     
+          //Load upload library
+          $this->load->library('upload',$config); 
+     
+          // File upload
+          if($this->upload->do_upload('file')){
+            // Get data about the file
+            $uploadData = $this->upload->data();
+          }
+          
+        }
+        redirect('issue/add','refresh'); 
+     
+      }
+
+    
+    
+}
+
--- application/views/issue/manage.php
+++ application/views/issue/manage.php
@@ -5,26 +5,35 @@
 
               <div class="card">
                 <div class="card-header card-header-rose">
-                  <h4 class="card-title ">User Table  <a href="<?php echo base_url()?>issue/add"><button class="btn btn-success ">ADD</button></a></h4>
-                  
-                  <p class="card-category"><h1></h1></p>
+                  <h4 class="card-title ">User Table  <a href="<?php echo base_url()?>user/add"><button class="btn btn-success ">ADD</button></a></h4>
+
                 </div>
                 <div class="card-body">
                   <div class="table-responsive">
                 <?php echo $this->session->flashdata("success"); ?>
-                  <table id="example" class="table ">
+                <?php echo form_open('#', array('id' => 'frm_usermanagement', 'name'=>'frm_usermanagement', 'class'=>'form-horizontal'));?>
+                  <table id="dynamic-table" class="table ">
                   <thead>
                     <tr>
-                      <th>Case No.</th>
-                      <th>Project</th>
-                        <th>Plant</th>
-                        <th>Curent Status</th>
-                        <th>Issue Descriptions</th>
-                        <th>Priorty</th>
-                        <th>Date Identifiled</th>
-                        <th>Date Expect Solution</th>
-                        <th>Date Entered</th>
-                        <th class="text-center" width="20%">Manage</th>
+								    	<td colspan="7">
+									    	<div id="btn_enable" class="btn  btn-success"><span class="fa fa-check"></span></div>
+									    	<div id="btn_disable" class="btn  btn-danger"><span class="fa fa-times"></span></div>
+									    	<div id="btn_delete" class="btn btn-dark"><span class="fa fa-trash-o"></span></div>
+								    	</td>
+								    </tr>	
+                    <tr class="table-dark text-dark">
+                    <th class="text-center">
+															<label class="pos-rel">
+																<input type="checkbox" class="ace" />
+																<span class="lbl"></span>
+															</label>
+														</th>
+                      <th  style="font-weight: bold;">Username</th>
+                      <th  style="font-weight: bold;">Name</th>
+                        <th  style="font-weight: bold;">Gender</th>
+                        <th  style="font-weight: bold;">Group</th>
+                        <th  style="font-weight: bold;">Email</th>
+                        <th  style="font-weight: bold;" class="text-center" width="20%">Manage</th>
                        
                       </tr>
                     </thead>
@@ -33,46 +42,51 @@
                     <?php
                     foreach($result as $r){
                  echo "<tr>";
-                echo "<td>"."</td>";
-                echo "<td>".$r->pj_name."</td>";
-                echo "<td>".$r->plant."</td>"; 
-                echo "<td>".$r->cur_st."</td>";
-                echo "<td>".$r->priority."</td>";
-                echo "<td>".$r->date_identified."</td>";
-                echo "<td>".$r->date_er."</td>";
-                echo "<td>".$r->date_updated."</td>";
+                 echo "<td style='text-align:center;'>
+                 <label class='pos-rel'>
+                     <input type='checkbox' class='ace' name='chk_uid[]' value='$r->su_id'/>
+                     <span class='lbl'></span>
+                   </label>
+               </td>";
+                echo "<td>".$r->username."</td>";
+                echo "<td>".$r->firstname." ".$r->lastname."</td>";
+                echo "<td>".$r->gender."</td>"; 
+                echo "<td>".$r->name."</td>";
+                echo "<td>".$r->email."</td>";
                 if($r->enable!=1 ){?>
-                  <!-- <td><a href='".base_url()."index.php/user/permission/".$r->user_id."' class='btn btn-danger'>Disable</a>"; -->
+               
                   <td class="text-center"><a type="button" data-toggle='tooltip' data-html='true' data-placement='bottom' aria-describedby='passHelp' title='<h5>เปิดการใช้งาน</h5>' data-original-title='Rule' onclick="javascript:window.location='<?php
-                  echo base_url() . 'user/enable/' . $r->is_id;
+                  echo base_url() . 'user/enable/' . $r->su_id;
                   ?>';"><i class='btn-danger btn-sm fa fa-times'></i></a>
                   <?php
                 }
                 else{?>
-                  <!-- echo "<td><a href='".base_url()."index.php/user/permission/".$r->user_id."' class='btn btn-success'>Enable</a>"; -->
+        
                   <td class="text-center"><a type="button" data-toggle='tooltip' data-html='true' data-placement='bottom' aria-describedby='passHelp' title='<h5>ปิดการใช้งาน</h5>'  data-original-title='Rule' onclick="javascript:window.location='<?php
-                  echo base_url() . 'user/disable/' . $r->is_id;
+                  echo base_url() . 'user/disable/' . $r->su_id;
                   ?>';"><i class='btn-success btn-sm fa fa-check'></i></a>                      
                   <?php
                 }
                 ?> 
 
 
-                <a class='btn-primary' data-toggle='tooltip' data-html='true' data-placement='bottom' aria-describedby='passHelp' title='<h5>เเก้ไขสิทธิ์</h5>' onclick="javascript:window.location='<?php
-                echo base_url() . 'user/rule/' . $r->is_id;
+                <a class='btn-primary' data-toggle='tooltip'  onclick="javascript:window.location='<?php
+                echo base_url() . 'user/rule/' . $r->su_id;
                 ?>';"><i class='btn-info btn-sm fa fa-gear'> </i></a>
 
 
-                <a type ='button' data-toggle='tooltip' data-html='true' data-placement='bottom' aria-describedby='passHelp' title='<h5>เเก้ไขข้อมูล</h5>' class=' ' data-original-title='Rule' onclick="javascript:window.location='<?php
-                echo base_url() . 'user/edit_u/' . $r->is_id;
+                <a type ='button' data-toggle='tooltip'  class=' ' onclick="javascript:window.location='<?php
+                echo base_url() . 'user/edit_u/' . $r->su_id;
                 ?>';"><i class='btn-info btn-sm fa fa-wrench'></i></a>
-                <?php 
-                echo "<a type='button' data-toggle='tooltip' data-html='true' data-placement='bottom' aria-describedby='passHelp' title='<h5>ลบข้อมูล</h5>' href='".base_url()."user/deleteuser/".$r->is_id."' onclick='return confirm(\"Confirm Delete Item\")' ><i class='btn-danger btn-sm fa fa-trash'></i></a></td>";  
-           
+
+                  <a type ='button' data-toggle='tooltip'  class=' ' onclick="if (confirm('Are you sure you want to delete '))javascript:window.location='<?php
+                echo base_url() . 'user/deleteuser/' . $r->su_id;
+                ?>';"><i class='btn-dark btn-sm fa fa-trash'></i></a>
+                <?php  
             echo "</tr>";
         }
     ?>
-                  
+                  	<?php echo form_close();?>
                     </tbody>
                   </table>
                 </div>
@@ -83,30 +97,14 @@
       </div>
 
             <script type="text/javascript">
-      
-      $(document).ready(function() {
-        
-        $('#table').DataTable({
-          dom: 'Bfrtip',
-        buttons: [
-            'colvis'
-        ]
-       
-    });  
-    });
-    
+
     $(document).ready(function () {
   //Pagination numbers
   $('#paginationSimpleNumbers').DataTable({
     "pagingType": "simple_numbers"
   });
-});
 
-</script>
-
-<script>
-$(document).ready(function() {
-$('#example').DataTable();
+ 
 } );
 </script>
     
