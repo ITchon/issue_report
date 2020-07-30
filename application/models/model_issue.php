0a67ab04c3a1fe2d1cefb34786a823fb8a851f47
Taisoul <64573602+Taisoul@users.noreply.github.com>
2020-07-30 08:20:38 +0700

    a

--- application/models/model_issue.php
+++ application/models/model_issue.php
@@ -84,4 +84,24 @@ public function delete_project($id)
 
 
 
+   public function sort_month($data){
+     $array=[];
+    for($i=1;$i<=12;$i++){
+      if (in_array($i,array_column($data, 'month')))
+      {           
+          foreach($data as $r){
+          if($i == $r->month){
+            array_push($array,$r->total);
+               }   
+            }
+      }
+        else
+         {   
+        array_push($array,0);
+          }
+      
+       }
+
+      return $array;
+       }
 }
